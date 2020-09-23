a2cc4611386232166339b3a538f052f2ddd196d2
Trandafir Emanuel <emanueltrandafir@yahoo.com>
2020-09-24 00:24:36 +0300

    created view package

--- view/Display.py
+++ view/Display.py
@@ -1,3 +1,40 @@
+import os
 
 
+class MenuScreen:
 
+    def __init__(self):
+        self.header = "Blackjack Night"
+        self.question = "press enter to continue.."
+        self.width = 80
+        
+    def with_header(self, header):
+        self.header = header 
+        return self
+        
+    def with_question(self, question):
+        self.question = question
+        return self
+        
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+    def build(self):
+        print(self.padding_vertical(1))
+        print(self.centered_text(self.header))
+        print(self.padding_vertical(1)) 
+        print(self.horizointal_line())
+        print(self.padding_vertical(11)) 
+        print(self.question)
+        input()
+        
+    def centered_text(self, text):
+        return int((self.width - len(text))/2)*" " + text
+    
+    def horizointal_line(self):
+        return "-"*self.width
+
+    def padding_vertical(self, times):
+        return "\n"*times
+
+s = MenuScreen().with_header("test").with_question("press any key").build()
